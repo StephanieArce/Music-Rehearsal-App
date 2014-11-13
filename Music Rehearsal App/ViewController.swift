c33de07983591fc7eb4e2325adc5b48184bb9eb1
StephanieArce <VJ@imacmanzo.wpi.edu>
2014-11-13 00:31:23 -0500

    Add Start and Stop Buttons to control timer

--- Music Rehearsal App/ViewController.swift
+++ Music Rehearsal App/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    // Button Controls
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    //Variables accessed to all functions
+    var timer = NSTimer()
+    var timeElapsed = 1.000
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+            
+        // Rounds out the Corners of Buttons
+        self.startButton.layer.cornerRadius = 5.0
+        self.stopButton.layer.cornerRadius = 5.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+   
+    @IBAction func didPushStartButton(sender: AnyObject) {
+        
+        //NSTimer will update every 1 second
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "timeTracker", userInfo: NSDate(), repeats: true)
+        
+        //Let's the start button appear
+        startButton.hidden = true
+        stopButton.hidden = false
+    }
+  
+    func timeTracker() {
+        
+        // This will keep track of the seconds passing by
+        var elapsed = -(self.timer.userInfo as NSDate).timeIntervalSinceNow
+        timeElapsed = elapsed
+    }
+    
+    @IBAction func didPushStopButton(sender: AnyObject) {
+        
+        timer.invalidate() // Stops the timer from repeating
+        
+        startButton.hidden = false
+        stopButton.hidden = true
+        
+        println("\(timeElapsed)")
+        
+    }
+    
 }
+
+
 
